4c6c5085f5c5577f98d447883c2d11bfef889087
adamrobertreid <adamrobertreid@gmail.com>
2016-11-15 16:34:14 -0800

    removed image constrant to canvas size

--- scripts/app.js
+++ scripts/app.js
@@ -34,8 +34,8 @@ function handleImage(e){
   reader.onload = function(event){
     var img = new Image();
     img.onload = function(){
-      canvas.width = img.width;
-      canvas.height = img.height;
+      // canvas.width = img.width;
+      // canvas.height = img.height;
       context.drawImage(img, 0,0);
     }
     img.src = event.target.result
